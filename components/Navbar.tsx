2af12ec9e8561a9657cdb5dd5090941b8d6ec853
Or Hasson <hassonor@gmail.com>
2024-08-05 21:16:07 +0300

    add dropdownmenu  with the Avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from '../img/logo.png';
+import {Avatar, AvatarImage, AvatarFallback} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
     return <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
         <Link href='/'>
             <Image src={logo} alt='OrHassonPress' width={40}/>
         </Link>
+        <DropdownMenu>
+            <DropdownMenuTrigger className='focus:outline-none'>
+                <Avatar>
+                    <AvatarImage src='https://github.com/hassonor.png' alt='@hassonor'/>
+                    <AvatarFallback className='text-black'>OH</AvatarFallback>
+                </Avatar></DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem><Link href='/profile'>
+                    Profile
+                </Link></DropdownMenuItem>
+                <DropdownMenuItem><Link href='/auth'>Logout</Link></DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     </div>
 }
 
