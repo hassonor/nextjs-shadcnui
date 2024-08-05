872225d110dff2e4201a99a25f8bca1866e65a66
Or Hasson <hassonor@gmail.com>
2024-08-05 21:47:22 +0300

    adding Dashboard card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import DashboardCard from "@/components/dashboard/DashboardCard";
+
 export default function Home() {
     return (<>
-            <h1 className="text-2xl">Dashboard</h1>
+            <div className="flex flex-col md:flex-row justify-between gap-5 mb">
+                <DashboardCard/>
+            </div>
         </>
     )
 }
