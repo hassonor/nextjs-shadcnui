052443cac5993278a244b996e949d25b17674ed6
Or Hasson <hassonor@gmail.com>
2024-08-06 11:31:57 +0300

    add main and auth layouts

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({children}: { children: React.ReactNode }) => {
+    return <div className="h-[100vh] flex items-center justify-center relative">
+        {children}
+    </div>
+}
+
+export default AuthLayout;
