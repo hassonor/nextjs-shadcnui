266fbf2622f695422b0f947455108204e9c9d709
Or Hasson <hassonor@gmail.com>
2024-08-06 11:18:28 +0300

    add theme toggler for dark/light/system modes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,27 +10,32 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import ThemeToggler from "@/components/ThemeToggler";
 
 const Navbar = () => {
     return <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
         <Link href='/'>
             <Image src={logo} alt='OrHassonPress' width={40}/>
         </Link>
-        <DropdownMenu>
-            <DropdownMenuTrigger className='focus:outline-none'>
-                <Avatar>
-                    <AvatarImage src='https://github.com/hassonor.png' alt='@hassonor'/>
-                    <AvatarFallback className='text-black'>OH</AvatarFallback>
-                </Avatar></DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator/>
-                <DropdownMenuItem><Link href='/profile'>
-                    Profile
-                </Link></DropdownMenuItem>
-                <DropdownMenuItem><Link href='/auth'>Logout</Link></DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="div flex items-center">
+            <ThemeToggler/>
+            <DropdownMenu>
+                <DropdownMenuTrigger className='focus:outline-none'>
+                    <Avatar>
+                        <AvatarImage src='https://github.com/hassonor.png' alt='@hassonor'/>
+                        <AvatarFallback className='text-black'>OH</AvatarFallback>
+                    </Avatar></DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuItem><Link href='/profile'>
+                        Profile
+                    </Link></DropdownMenuItem>
+                    <DropdownMenuItem><Link href='/auth'>Logout</Link></DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+
     </div>
 }
 
